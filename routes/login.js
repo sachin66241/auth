7734e07ca00a2ae8266cbb249ddedbc6ac498003
sachin <sachin6624@gmail.com>
2020-05-27 12:29:52 +0530

    sha256 encryption added for password

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Login = require('../controller/login')
+const sha256 = require('sha256');
+
 
 
 /* GET home page. */
@@ -19,7 +21,10 @@ router.post('/login', async(req, res, next) => {
   const {password} = req.body;
   const {email} = req.body;
   const entry = new Login();
-  var result = await entry.login(email,password);
+
+  /*SHA256 Encryption for password */
+  const encrypted_password = sha256(password);
+  var result = await entry.login(email,encrypted_password);
   if(!result){
     res.render('login',{status:"0"})
   }
--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Signup = require('../controller/register')
-
+const sha256 = require('sha256')
 /* GET register form */
 router.get('/register', (req, res, next)=> {
   res.render('register',{status:"2"});
@@ -15,9 +15,11 @@ router.post('/register', async (req, res, next)=> {
   const {email} = req.body;
   const {password} = req.body;
   const {dob} = req.body;
+    /*SHA256 Encryption for password */
 
+  const encrypted_password = sha256(password)
   const entry = new Signup();
-  var result = await entry.signup(first_name,last_name,email,password,dob);
+  var result = await entry.signup(first_name,last_name,email,encrypted_password,dob);
   if(!result){
     res.render('register',{status:"0"})
   }
